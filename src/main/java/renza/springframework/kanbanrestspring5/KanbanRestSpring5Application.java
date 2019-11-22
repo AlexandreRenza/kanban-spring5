17ce275578dc15cbba60e38662c63d21e4c95f3d
AlexandreRenza <alexandrerenza@gmail.com>
2019-11-22 17:14:12 -0500

    ALL PACKAGE PROJECT

---
+++ src/main/java/renza/springframework/kanbanrestspring5/KanbanRestSpring5Application.java
@@ -0,0 +1,13 @@
+package renza.springframework.kanbanrestspring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanRestSpring5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanRestSpring5Application.class, args);
+    }
+
+}
---
+++ src/test/java/renza/springframework/kanbanrestspring5/KanbanRestSpring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package renza.springframework.kanbanrestspring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanRestSpring5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
