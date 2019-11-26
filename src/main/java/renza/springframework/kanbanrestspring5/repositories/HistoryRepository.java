355925f8a868e3d60a05ea7979d5b321a8410d14
AlexandreRenza <alexandrerenza@gmail.com>
2019-11-26 11:59:20 -0500

    Created Repository to History. Closes #12

---
+++ src/main/java/renza/springframework/kanbanrestspring5/repositories/HistoryRepository.java
@@ -0,0 +1,10 @@
+package renza.springframework.kanbanrestspring5.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import renza.springframework.kanbanrestspring5.domain.History;
+
+public interface HistoryRepository extends JpaRepository<History, Long> {
+
+    History finfByName(String Name);
+
+}
