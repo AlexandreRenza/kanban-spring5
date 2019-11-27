aaa3f398475e8e93b272f68a18bf9af4d9e0ec81
AlexandreRenza <alexandrerenza@gmail.com>
2019-11-27 11:23:16 -0500

    Created Controller to History. Closes #15

---
+++ src/main/java/renza/springframework/kanbanrestspring5/controller/HistoryController.java
@@ -0,0 +1,57 @@
+package renza.springframework.kanbanrestspring5.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import renza.springframework.kanbanrestspring5.api.v1.model.HistoryDTO;
+import renza.springframework.kanbanrestspring5.api.v1.model.HistoryListDTO;
+import renza.springframework.kanbanrestspring5.services.HistoryService;
+
+@RestController
+@RequestMapping(ProjectController.BASE_URL)
+@CrossOrigin(origins = "${originUrl}", methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+public class HistoryController {
+
+    public static final String BASE_URL = "api/v1/projects";
+
+    private final HistoryService historyService;
+
+
+    public HistoryController(HistoryService historyService) {
+        this.historyService = historyService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public HistoryListDTO getListOfHistoryDTO(){
+
+        return new HistoryListDTO(historyService.getAllHistories());
+
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public HistoryDTO getHistorybyId(@PathVariable Long id){
+        return historyService.getHistoryByID(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public HistoryDTO createNewHistory(@RequestBody HistoryDTO historyDTO){
+        return historyService.createNewHistory(historyDTO);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public HistoryDTO updateHistory(@PathVariable Long id, @RequestBody HistoryDTO historyDTO){
+        return historyService.saveHistoryByDto(id, historyDTO)  ;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteHistory(@PathVariable Long id){
+        historyService.deleteHistory(id);
+    }
+
+
+
+}
